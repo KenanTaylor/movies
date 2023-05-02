93e504867d16c276579fac22dd2772e38bc8d049
Hdesai97 <hdesai97@gmail.com>
2023-05-02 15:09:22 -0400

    changes made to homepage added trailer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Netflix from "./pages/Netflix";
+import Movie from "./pages/Movie";
+import Player from "./pages/Player";
 
 
 export default function App () {
@@ -11,7 +12,9 @@ export default function App () {
     <Routes>
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/signup" element={<Signup />} />
-      <Route exact path="/" element={<Netflix />} />
+      <Route exact path="/player" element={<Player />} />
+      <Route exact path="/" element={<Movie />} />
+     
 
     </Routes>
     
